c0b3104f8d938a62c1254b4e44409a51b9d9787f
lchthanhth615 <lchthanhth6152gmail.com>
2017-05-06 11:07:08 +0700

    create new maven project HelloMvnGit

---
+++ HelloMvnGit/src/main/java/kevin/lu/hello/com/App.java
@@ -0,0 +1,13 @@
+package kevin.lu.hello.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
